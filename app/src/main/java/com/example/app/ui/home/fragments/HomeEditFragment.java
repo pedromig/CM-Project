22e3d4c22883cc504ceafdf5d5d48577ff6b91e9
joanaa-b <81414109+joanaa-b@users.noreply.github.com>
2023-01-05 21:15:29 +0000

    Added photopicker to home edit

--- app/src/main/java/com/example/app/ui/home/fragments/HomeEditFragment.java
+++ app/src/main/java/com/example/app/ui/home/fragments/HomeEditFragment.java
@@ -1,12 +1,21 @@
 package com.example.app.ui.home.fragments;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
 
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
+import android.provider.MediaStore;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
@@ -16,6 +25,7 @@ import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.PopupMenu;
 import android.widget.Toast;
 
 import com.example.app.R;
@@ -110,6 +120,32 @@ public class HomeEditFragment extends Fragment {
             );
             toast.show();
         });
+
+        this.updateImage.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View view) {
+                PopupMenu popupMenu = new PopupMenu(getActivity(), updateImage);
+                popupMenu.getMenuInflater().inflate(R.menu.photo_edit_menu, popupMenu.getMenu());
+
+                popupMenu.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+                    public boolean onMenuItemClick(MenuItem item) {
+                        if (item.getItemId() == R.id.action_camera) {
+                            Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                            ImagePickerCamera.launch(intent);
+                        }
+                        if (item.getItemId() == R.id.action_gallery) {
+                            Intent intent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                            ImagePickerGallery.launch(intent);
+                        }
+                        return true;
+                    }
+                });
+                popupMenu.show();
+            }
+        });
+
+
         super.onViewCreated(view, savedInstanceState);
     }
 
@@ -126,4 +162,29 @@ public class HomeEditFragment extends Fragment {
         }
         return true;
     }
+
+    ActivityResultLauncher<Intent> ImagePickerCamera = registerForActivityResult(
+            new ActivityResultContracts.StartActivityForResult(),
+            new ActivityResultCallback<ActivityResult>() {
+                @Override
+                public void onActivityResult(ActivityResult result) {
+                    if (result.getResultCode() == Activity.RESULT_OK && result.getData() != null) {
+                        Bundle bundle = result.getData().getExtras();
+                        Bitmap bitmap = (Bitmap) bundle.get("data");
+                        picture.setImageBitmap(bitmap);
+                    }
+                }
+            });
+
+    ActivityResultLauncher<Intent> ImagePickerGallery = registerForActivityResult(
+            new ActivityResultContracts.StartActivityForResult(),
+            new ActivityResultCallback<ActivityResult>() {
+                @Override
+                public void onActivityResult(ActivityResult result) {
+                    if (result.getResultCode() == Activity.RESULT_OK && result.getData() != null) {
+                        Uri uri =  result.getData().getData();
+                        picture.setImageURI(uri);
+                    }
+                }
+            });
 }
